c745843fe3228de8e5b04f9e2c1a074860df1784
ch-dev02 <75454949+ch-dev02@users.noreply.github.com>
2023-02-02 12:36:09 +0000

    Initial Database Design Complete, Subject to future changes

--- files/app/__init__.py
+++ files/app/__init__.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_admin import Admin
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Please login to use this website.'
+login_manager.login_message_category = 'primary'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+admin = Admin(app, name='Admin', template_mode='bootstrap3')
+
 from app.views import auth
+from app import models
--- files/app/views/auth.py
+++ files/app/views/auth.py
@@ -1,5 +1,16 @@
-from app import app
+from app import app, db, models, admin
+from flask_admin.contrib.sqla import ModelView
 from flask import render_template, request, flash, redirect, url_for, Markup
+
+admin.add_view(ModelView(models.User, db.session))
+admin.add_view(ModelView(models.PassToken, db.session))
+admin.add_view(ModelView(models.Group, db.session))
+admin.add_view(ModelView(models.Member, db.session))
+admin.add_view(ModelView(models.Folder, db.session))
+admin.add_view(ModelView(models.Resource, db.session))
+admin.add_view(ModelView(models.Review, db.session))
+admin.add_view(ModelView(models.Keywords, db.session))
+admin.add_view(ModelView(models.Queue, db.session))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
